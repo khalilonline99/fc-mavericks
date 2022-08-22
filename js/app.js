b3e23cd26d583c4f00fc3a79db0488581dcbd747
khalilonline99 <khalilonline99@gmail.com>
2022-08-23 00:41:36 +0600

    comments added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,5 @@
 document.getElementById('btn-player-1').addEventListener('click',function(){
     selectPlayer('name-player-1', 'player-selected','btn-player-1');
-    /* const namePlayer = document.getElementById('name-player-1');
-    const namePlayerText = namePlayer.innerText;
-
-    const selectedField = document.getElementById('player-selected-one');
-    selectedField.innerText = namePlayerText;
-
-    if (selectedField.innerText === namePlayerText){
-        document.getElementById('btn-player-1').disabled = true;
-        document.getElementById('btn-player-1').style.backgroundColor = 'gray';
-        document.getElementById('btn-player-1').innerText = 'SELECTED';
-        
-    } */
 })
 
 document.getElementById('btn-player-2').addEventListener('click',function(){
