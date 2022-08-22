914d9edcd9d9dd4b22c0fb62eecce4f76f77cf25
khalilonline99 <khalilonline99@gmail.com>
2022-08-22 21:36:26 +0600

    players cost per person OK

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 document.getElementById('btn-player-1').addEventListener('click',function(){
-    const namePlayer = document.getElementById('name-player-1');
+    selectPlayer('name-player-1', 'player-selected','btn-player-1');
+    /* const namePlayer = document.getElementById('name-player-1');
     const namePlayerText = namePlayer.innerText;
 
     const selectedField = document.getElementById('player-selected-one');
@@ -10,35 +11,55 @@ document.getElementById('btn-player-1').addEventListener('click',function(){
         document.getElementById('btn-player-1').style.backgroundColor = 'gray';
         document.getElementById('btn-player-1').innerText = 'SELECTED';
         
-    }
+    } */
 })
 
-
 document.getElementById('btn-player-2').addEventListener('click',function(){
-    const namePlayer = document.getElementById('name-player-2');
-    const namePlayerText = namePlayer.innerText;
-
-    /* const selectedField = document.getElementById('player-selected-two');
-    selectedField.innerText = namePlayerText; */
-    const selectedField = document.getElementById('player-selected');
-    selectedField.innerHTML = selectedField.innerHTML + "<li>" + namePlayerText + "</li>";
-
-    if (selectedField.innerText === namePlayerText){
-        document.getElementById('btn-player-2').disabled = true;
-        document.getElementById('btn-player-2').style.backgroundColor = 'gray';
-        document.getElementById('btn-player-2').innerText = 'SELECTED';
-        
-    }
-    
+    selectPlayer('name-player-2', 'player-selected','btn-player-2');
 })
 
 document.getElementById('btn-player-3').addEventListener('click',function(){
-    const namePlayer = document.getElementById('name-player-3');
-    const namePlayerText = namePlayer.innerText;
-
-    const selectedField = document.getElementById('player-selected');
-    selectedField.innerHTML = selectedField.innerHTML + "<li>" + namePlayerText + "</li>";
-    
+    selectPlayer('name-player-3', 'player-selected', 'btn-player-3');
 })
 
-// test of pushs 
+document.getElementById('btn-player-4').addEventListener('click',function(){
+    selectPlayer('name-player-4', 'player-selected', 'btn-player-4');
+})
+
+document.getElementById('btn-player-5').addEventListener('click',function(){
+    selectPlayer('name-player-5', 'player-selected', 'btn-player-5');
+})
+
+document.getElementById('btn-player-6').addEventListener('click',function(){
+    selectPlayer('name-player-6', 'player-selected', 'btn-player-6');
+})
+document.getElementById('btn-player-7').addEventListener('click',function(){
+    selectPlayer('name-player-7', 'player-selected', 'btn-player-7');
+})
+document.getElementById('btn-player-8').addEventListener('click',function(){
+    selectPlayer('name-player-8', 'player-selected', 'btn-player-8');
+})
+document.getElementById('btn-player-9').addEventListener('click',function(){
+    selectPlayer('name-player-9', 'player-selected', 'btn-player-9');
+})
+
+
+document.getElementById('btn-calculate').addEventListener('click',function(){
+    const playerBudget = document.getElementById('player-budget');
+    const playerBudgetString = playerBudget.value;
+
+    if (isNaN(playerBudgetString) === false){
+        const playerBudgetNumber = parseInt(playerBudgetString);
+        const selectedPlayersNo = document.getElementById('player-selected').getElementsByTagName('li').length;
+        const totalAmount = selectedPlayersNo * playerBudgetNumber;
+
+        const totalAmountDisplay = document.getElementById('amount-total-player');
+        totalAmountDisplay.innerText = totalAmount;
+
+        console.log(totalAmount);
+    }
+    else{
+        alert('Please give numbers only');
+    }
+    
+})
---
+++ js/utilities.js
@@ -0,0 +1,25 @@
+function selectPlayer(playerName, playerSelected, buttonCardPlayer){
+
+    const lengthPlayers = document.getElementById('player-selected').getElementsByTagName('li').length;
+
+    if (lengthPlayers < 5){
+        const namePlayer = document.getElementById(playerName);
+        const namePlayerText = namePlayer.innerText;
+
+        const selectedField = document.getElementById(playerSelected);
+        selectedField.innerHTML = selectedField.innerHTML + "<li>" + namePlayerText + "</li>";
+        const playerButton = document.getElementById(buttonCardPlayer);
+
+        if (selectedField.lastChild.innerText === namePlayerText){
+            playerButton.disabled = true;
+            playerButton.style.backgroundColor = 'gray';
+            playerButton.innerText = 'SELECTED';
+            
+        }
+    }
+    else{
+
+        alert('Warning!! You cannot select more than Five Players');
+    }
+    
+}
