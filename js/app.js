088521c258629d891bd9774ac5cc33df4e3b6576
khalilonline99 <khalilonline99@gmail.com>
2022-08-22 18:08:39 +0600

    adding javascript player select option

---
+++ js/app.js
@@ -0,0 +1,42 @@
+document.getElementById('btn-player-1').addEventListener('click',function(){
+    const namePlayer = document.getElementById('name-player-1');
+    const namePlayerText = namePlayer.innerText;
+
+    const selectedField = document.getElementById('player-selected-one');
+    selectedField.innerText = namePlayerText;
+
+    if (selectedField.innerText === namePlayerText){
+        document.getElementById('btn-player-1').disabled = true;
+        document.getElementById('btn-player-1').style.backgroundColor = 'gray';
+        document.getElementById('btn-player-1').innerText = 'SELECTED';
+        
+    }
+})
+
+
+document.getElementById('btn-player-2').addEventListener('click',function(){
+    const namePlayer = document.getElementById('name-player-2');
+    const namePlayerText = namePlayer.innerText;
+
+    /* const selectedField = document.getElementById('player-selected-two');
+    selectedField.innerText = namePlayerText; */
+    const selectedField = document.getElementById('player-selected');
+    selectedField.innerHTML = selectedField.innerHTML + "<li>" + namePlayerText + "</li>";
+
+    if (selectedField.innerText === namePlayerText){
+        document.getElementById('btn-player-2').disabled = true;
+        document.getElementById('btn-player-2').style.backgroundColor = 'gray';
+        document.getElementById('btn-player-2').innerText = 'SELECTED';
+        
+    }
+    
+})
+
+document.getElementById('btn-player-3').addEventListener('click',function(){
+    const namePlayer = document.getElementById('name-player-3');
+    const namePlayerText = namePlayer.innerText;
+
+    const selectedField = document.getElementById('player-selected');
+    selectedField.innerHTML = selectedField.innerHTML + "<li>" + namePlayerText + "</li>";
+    
+})
